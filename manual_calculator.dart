a2f206f4d340de75bbd0c12f6ac6ea1682be385c
Aakash Agarwal <aakash@instrik.com>
2024-07-10 00:45:24 +0530

    Update manual_calculator.dart

--- manual_calculator.dart
+++ manual_calculator.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'package:Bmi/screens/input_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
