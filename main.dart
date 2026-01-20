899587990a7f72b12dbe43533cd48518fe025bfd
Aakash Agarwal <agarwal7409@gmail.com>
2026-01-21 00:41:03 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -3,7 +3,7 @@
 import 'package:flutter/material.dart';
 import 'screens/input_page.dart';
  
-//trial  jkjmjjjkkmpp;
+//trial h
 
 void main() {
   runApp(Bmi());
@@ -27,6 +27,7 @@ class _BmiState extends State<Bmi> {
     );
   }
 }
+
 
 
 
